83d327cd80c9a43593b50d5fca4835caf7a8da8c
palakpatel125477-stack <palak.patel125477@marwadiuniversity.ac.in>
2026-06-09 10:13:41 +0530

    Add files via upload

---
+++ 2nd.php
@@ -0,0 +1,33 @@
+<?php
+
+
+echo "<center>";
+
+
+echo "==================================================<br>";
+echo "MARWADI UNIVERSITY<br>";
+echo "STUDENT RESULT<br>";
+echo "==================================================<br><br>";
+
+
+echo "Roll No: 7051<br>";
+echo "Name: Palak Patel<br>";
+echo "Semester: 2nd Semester<br>";
+echo "--------------------------------------------------<br><br>";
+
+
+echo "PHP Programming: 90 / 100<br>";
+echo "Database Systems: 92 / 100<br>";
+echo "Data Structures: 85 / 100<br>";
+echo "--------------------------------------------------<br><br>";
+
+
+echo "Total Marks: 280 / 300<br>";
+echo "Percentage: 91.33%<br>";
+echo "Result Status: PASSED<br>";
+echo "==================================================<br>";
+
+
+echo "</center>";
+
+?>
